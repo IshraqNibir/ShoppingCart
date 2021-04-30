b6d5a7071e2fedc6e0d66d84829de77007535b7c
ishraq <ishraq.nibir@gmail.com>
2021-04-30 23:00:16 +0600

    Backend For Order Done

--- shopapp/models.py
+++ shopapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    product_name = models.CharField(max_length=200)
+    quantity = models.PositiveIntegerField(blank=False)
+    product_code = models.CharField(max_length=200, blank=False)
+    category = models.CharField(max_length=200, blank=False)
+    unit_price = models.PositiveIntegerField(blank=False)
+
+    def __str__(self):
+        return self.product_name
+
+class Order(models.Model):
+    email = models.EmailField(blank=False)
+    phone = models.CharField(max_length=20, blank=False)
+    name = models.CharField(max_length=100, blank=False)
+    product = models.CharField(max_length=100, blank=False)
+    quantity = models.PositiveIntegerField(blank=False)
--- shopapp/views.py
+++ shopapp/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Order
+from .forms import OrderForm
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 def home(request):
     return render(request, 'shopapp/home.html')
 
+@csrf_exempt
 def order(request):
-    return render(request, 'shopapp/order.html')
+    form = OrderForm()
+    message = ""
+    if request.method == "POST":
+        data = request.POST
+        f = OrderForm(request.POST)
+        if f.is_valid():
+            order_data = f.save()
+        message = "Product Added To The Cart"
+    return render(request, 'shopapp/order.html', {"message": message})
